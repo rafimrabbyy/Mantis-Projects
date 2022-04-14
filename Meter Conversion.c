9311861dec6845ec0b11f01b0833cd1b88fbbe67
rafimrabbyy <102420300+rafimrabbyy@users.noreply.github.com>
2022-04-14 15:49:31 +0600

    Add files via upload

---
+++ Meter Conversion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    float centimeter;
+
+        printf("Enter length in centimeter = ");
+        scanf("%f",&centimeter);
+        printf("Length in meter =%.3f\n",centimeter/100); //specified decimals for visual preference
+       printf("Length in kilometer =%.3f",centimeter/1000);
+
+    return 0;
+}
