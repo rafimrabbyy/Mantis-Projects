f35f74f8c0059d9830e4909a0235a5775b53caa7
rafimrabbyy <102420300+rafimrabbyy@users.noreply.github.com>
2022-04-14 20:32:26 +0600

    Update Length Conversion.c

--- Length Conversion.c
+++ Length Conversion.c
@@ -7,7 +7,7 @@ int main()
         printf("Enter length in centimeter = ");
         scanf("%f",&centimeter);
         printf("Length in meter =%.3f\n",centimeter/100); //specified decimals for visual preference
-       printf("Length in kilometer =%.3f",centimeter/1000);
+       printf("Length in kilometer =%.3f",centimeter/100000);
 
     return 0;
 }
