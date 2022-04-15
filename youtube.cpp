48b55d84f414485dbbf510b0048c98f973b75752
rafimrabbyy <102420300+rafimrabbyy@users.noreply.github.com>
2022-04-15 21:43:49 +0600

    Add files via upload

---
+++ youtube.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using std::cout;
+
+using std::cin;
+
+int main()
+
+{
+
+int A;
+
+int B;
+
+int R;
+
+cin >>A>>B;
+
+if(A>B)
+
+{
+
+R = A%B;
+
+}
+
+else
+
+{
+
+R = B%A;
+
+}
+
+if(R == 0)
+
+{
+
+cout<<"Sao Multiplos";
+
+}
+
+else
+
+{
+
+cout<<"Nao sao Multiplos";
+
+}
+
+return 0;
+}
