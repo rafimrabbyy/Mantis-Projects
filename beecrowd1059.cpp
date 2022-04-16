6afc786717ce5659d9c4acd1e63da428924f7f28
rafimrabbyy <102420300+rafimrabbyy@users.noreply.github.com>
2022-04-16 13:34:05 +0600

    Add files via upload

---
+++ beecrowd1059.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+int main()
+{
+          /*
+        Team Mantis!
+        Solving problem 4;
+    */
+int x = 0;
+    for(int i=0; i < 100; i++)
+        {
+            x = x+1;
+            cout<<x<<"\n";
+        }
+return 0;
+}
